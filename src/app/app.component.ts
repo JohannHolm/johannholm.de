34700a5c375ed53e1143b7591cae1031a1da5bc5
JohannHolm <johannholm94@gmx.de>
2023-11-18 19:10:29 +0100

    added images + slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
-export class AppComponent {
-  title = 'johannholm';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.updateImage();
+  }
+  images: string[] = [
+    'assets/img/1.png',
+    'assets/img/2.png',
+    'assets/img/3.png',
+  ];
+  currentImage: number = 0;
+  showImage: boolean = true;
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 5000);
+  }
 }
